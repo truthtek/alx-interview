f069173a0d3210ea33511b0a701566efcd0d178e
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-07-17 09:57:19 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,29 @@
 #!/usr/bin/python3
-
 import sys
+import re
 
-
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
+def print_msg(status_code_counts, total_size):
+    print("File size: {}".format(total_size))
+    for key, val in sorted(status_code_counts.items()):
         if val != 0:
             print("{}: {}".format(key, val))
 
-
-total_file_size = 0
-code = 0
+status_code_counts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+total_size = 0
 counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
-
-        if len(parsed_line) > 2:
+        parsed_line = re.match(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)', line)
+        if parsed_line:
             counter += 1
-
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
-
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
-
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
-
-finally:
-    print_msg(dict_sc, total_file_size)
+            total_size += int(parsed_line.group(4))
+            code = parsed_line.group(3)
+            if code in status_code_counts:
+                status_code_counts[code] += 1
+        if counter == 10:
+            print_msg(status_code_counts, total_size)
+            counter = 0
+    print_msg(status_code_counts, total_size)
+except Exception as e:
+    print("Error: {}".format(e))

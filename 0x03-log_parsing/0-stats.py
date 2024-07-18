468a51f81c9008d43bac5964ac80cd70c4650a92
Lukman Adedokun <126268337+truthtek@users.noreply.github.com>
2024-07-18 16:29:18 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,29 +1,39 @@
 #!/usr/bin/python3
+"""
+Log parsing
+"""
+
 import sys
-import re
 
-def print_msg(status_code_counts, total_size):
-    print("File size: {}".format(total_size))
-    for key, val in sorted(status_code_counts.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+if __name__ == '__main__':
 
-status_code_counts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-total_size = 0
-counter = 0
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-try:
-    for line in sys.stdin:
-        parsed_line = re.match(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] "GET /projects/260 HTTP/1.1" (\d+) (\d+)', line)
-        if parsed_line:
-            counter += 1
-            total_size += int(parsed_line.group(4))
-            code = parsed_line.group(3)
-            if code in status_code_counts:
-                status_code_counts[code] += 1
-        if counter == 10:
-            print_msg(status_code_counts, total_size)
-            counter = 0
-    print_msg(status_code_counts, total_size)
-except Exception as e:
-    print("Error: {}".format(e))
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
+
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, filesize)
+        raise
